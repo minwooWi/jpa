f9431c71b1f1cd5f74907c458bfde2ae7cea1926
Minwoo Chase <c5287@nate.com>
2023-07-12 12:36:37 +0900

    modify userName

--- src/main/java/xyz/kbootcamp/web/IndexController.java
+++ src/main/java/xyz/kbootcamp/web/IndexController.java
@@ -24,6 +24,7 @@ public class IndexController {
         if (user != null) {
             model.addAttribute("userName", user.getName());
             model.addAttribute("userEmail", user.getEmail());
+            model.addAttribute("accountName", user.getName());
         }
 
         return "index";
